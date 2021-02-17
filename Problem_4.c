c5e5a849b5351a09323c4f98130b29d5b08b6089
ShreyaAR16 <arshreya15@gmail.com>
2021-02-17 12:20:31 +0000

    star

--- Problem_4.c
+++ Problem_4.c
@@ -11,6 +11,17 @@ int main()
 
   	//Write your code here
   	
+    int i,j;
+    for (i=0;i<n;i++)
+    {
+      for (j=0;j<=i;j++)
+    {
+    printf("*");
+}
+    printf("\n");
+
+    }
+
 
   	return 0;
 }
